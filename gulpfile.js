e9a2cac48d8919d7d15ac24d755513b2316c3e92
Tony Cosentini <tony@endlesswhileloop.com>
2015-05-08 08:55:28 -0700

    More cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
 var gulp = require('gulp');
 var del = require('del');
 var browserify = require('browserify');
-var watchify = require('watchify');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
@@ -39,6 +36,8 @@ gulp.task('js', ['copyDependencies'], function() {
     return b.bundle()
         .pipe(source(config.out))
         .pipe(buffer())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(config.dest));
 });
 
--- src/script.js
+++ src/script.js
@@ -1,76 +1,125 @@
 var xhr = require('xhr');
 
 // Fetch crash ID.
-var crashIDRegex = /https:\/\/fabric.io\/.*\/([a-zA-Z0-9]*)/;
-var crashID = crashIDRegex.exec(window.location.href)[1];
+var crashIdRegex = /https:\/\/fabric.io\/.*\/([a-zA-Z0-9]*)/;
+var crashId = crashIdRegex.exec(window.location.href)[1];
 
-// Fetch appID;
-var appIDStringRegex = /https:\/\/fabric.io\/.*\/apps\/([A-Za-z0-9.-]*)/;
-var appIDString = appIDStringRegex.exec(window.location.href)[1];
-
-var appID;
-
-function getAppData(developerToken, callback) {
-  xhr({
-    uri: 'https://fabric.io/api/v2/apps?include=sdk_kit',
-    headers: {
-      "X-CRASHLYTICS-DEVELOPER-TOKEN": developerToken
-    },
-    json: true
-  }, function(error, response, body) {
-    for (var i in response.body) {
-      var app = response.body[i];
-      if (app.bundle_identifier == appIDString) {
-        appID = app.id;
-        callback();
-        break;
-      }
-    }
-  });
+if (!crashId) {
+  exit();
 }
+
+var DeveloperTokenFetcher = new function() {
+  var listeners = [];
+  var developerToken;
+
+  var notifyListeners = function() {
+    for (i = 0; i < listeners.length; i++) {
+      listeners[0](developerToken);
+    }
+  };
+
+  return {
+    addListener: function(listener) {
+      if (developerToken != null) {
+        listener(developerToken);
+      }
+      listeners.push(listener);
+    },
+    fetch: function () {
+      xhr({
+        uri: 'https://fabric.io/api/v2/client_boot/config_data',
+        json: true
+      }, function (error, response, body) {
+        developerToken = response.body.developer_token;
+        notifyListeners();
+      });
+    }
+  };
+};
+
+var AppDataFetcher = new function() {
+  var appIdStringRegex = /https:\/\/fabric.io\/.*\/apps\/([A-Za-z0-9.-]*)/;
+  var appIdString = appIdStringRegex.exec(window.location.href)[1];
+
+  var listeners = [];
+  var appId;
+
+  var notifyListeners = function() {
+    for (i = 0; i < listeners.length; i++) {
+      listeners[0](appId);
+    }
+  };
+
+  return {
+    addListener: function(listener) {
+      if (appId != null) {
+        listener(appId);
+      }
+      listeners.push(listener);
+    },
+    fetch: function(developerToken) {
+      xhr({
+        uri: 'https://fabric.io/api/v2/apps?include=sdk_kit',
+        headers: {
+          "X-CRASHLYTICS-DEVELOPER-TOKEN": developerToken
+        },
+        json: true
+      }, function(error, response, body) {
+        for (var i in response.body) {
+          var app = response.body[i];
+          if (app.bundle_identifier == appIdString) {
+            appId = app.id;
+            notifyListeners();
+            break;
+          }
+        }
+      });
+    }
+  };
+};
+
+DeveloperTokenFetcher.addListener(function (developerToken) {
+  AppDataFetcher.fetch(developerToken);
+});
+DeveloperTokenFetcher.fetch();
 
 document.arrive("td.details", function() {
   var tdElement = this;
 
-  // Get an access token.
-  xhr({
-    uri: 'https://fabric.io/api/v2/client_boot/config_data',
-    json: true
-  }, function(error, response, body) {
-    var developerToken = response.body.developer_token;
-    getAppData(developerToken, function() {
-      loadCommentsForRelatedIssue(tdElement);
-    });
+  AppDataFetcher.addListener(function(appId) {
+    loadCommentsForRelatedIssue(tdElement, appId);
   });
 });
 
-function loadCommentsForRelatedIssue(relatedIssueTd) {
-  var crashID = crashIDRegex.exec(relatedIssueTd.firstElementChild['href'])[1];
+function loadCommentsForRelatedIssue(relatedIssueTd, appId) {
+  var crashID = crashIdRegex.exec(relatedIssueTd.firstElementChild['href'])[1];
 
   xhr({
-    uri: "https://fabric.io/api/v3/projects/" + appID + "/issues/" + crashID + "/notes",
+    uri: "https://fabric.io/api/v3/projects/" + appId + "/issues/" + crashID + "/notes",
     json: true
   }, function(error, response, body) {
-    var bold = document.createElement('strong');
-    var text = document.createTextNode("Comments");
+    // Is it possible to do this in a cleaner way without adding jQuery?
     var newline = document.createElement('br');
+    relatedIssueTd.appendChild(newline);
+    var bold = document.createElement('strong');
+    var text;
+    if (response.body.length == 0) {
+      text = document.createTextNode("No Comments");
+    } else {
+      text = document.createTextNode("Comments");
+    }
     bold.appendChild(text);
     relatedIssueTd.appendChild(bold);
+    newline = document.createElement('br');
     relatedIssueTd.appendChild(newline);
-
+    
     for (var i in response.body) {
       var note = response.body[i];
         if (note != null) {
-          console.log(note.body);
-          console.log(note.account.email);
-          console.log(note.account.name);
-
           var comment = document.createTextNode(note.body +   " - " + note.account.name + " / " + note.account.email);
 
           var newline = document.createElement('br');
-
           relatedIssueTd.appendChild(comment);
-          relatedIssueTd.appendChild(newline);
           relatedIssueTd.appendChild(newline);
         }
     }
